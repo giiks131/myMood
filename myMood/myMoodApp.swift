e6827670a7a00102b149bf7fb8dde771d1712b48
Alex Sh <giiks131@gmail.com>
2022-10-24 09:37:38 +0200

    Initial Commit

---
+++ myMood/myMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMoodApp.swift
+//  myMood
+//
+//  Created by Aleksandr Shapovalov on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct myMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
