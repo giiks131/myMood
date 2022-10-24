8678ccb269560665bea69a7ad32338166a5348e5
Alex Sh <giiks131@gmail.com>
2022-10-24 12:58:57 +0200

    #1.1 a little amount of code for a main screen

--- myMood/Views/MoodView.swift
+++ myMood/Views/MoodView.swift
@@ -3,11 +3,47 @@ import SwiftUI
 
 struct MoodView: View {
     var body: some View {
-        VStack {
+     
+        NavigationStack {
+            TabView {
+                
+                
+                VStack(alignment: .leading){
+                    Text("Today Ring")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                    Image("emoji")
+                        .resizable()
+                        
+                        .frame(width: 200, height: 200)
+                    Text("Week Ring")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                    Image("ring")
+                        .resizable()
+                        .frame(width: 150, height: 150, alignment: .center)
+                    List {
+                        Text("hello 1")
+                        Text("hello 2")
+                        Text("hello 3")
+                        
+                        
+                    }
+                    .background(Color("appColor"))
+                    .scrollContentBackground(.hidden)
+                    
+                    
+                    
+                }
+                
+                .navigationTitle("Mood")
+                .padding()
+                .background(Color("appColor"))
 
-
+            }
+        
         }
-        .padding()
+        
     }
 }
 
